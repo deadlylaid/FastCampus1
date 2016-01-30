0d8104ffe1ae9d822a1490ebb0c75d0a4723cba1
deadlylaid <deadlylaid@gmail.com>
2016-01-31 00:19:25 +0900

    Stack_bracket

---
+++ HanMinsoo/4th_week_DataStructure/Stack_bracket.py
@@ -0,0 +1,26 @@
+from 자료구조_스택구현 import ArrayStack
+a = '((()(){([()])}))'
+cd = '((({{}})))'
+b= '()(()){([()])}'
+c=')(()){([()])}'
+d='(}'
+
+S=ArrayStack()
+list_a = []
+for i in a:
+    if i=='(' or i=='{' or i=='[':
+        S.push(i)
+        print("1",S.data)
+    elif i==')' and S.top()=='(':
+        S.pop()
+        print("2",S.data)
+    elif i=='}' and S.top()=='{':
+        S.pop()
+        print("3",S.data)
+    elif i==']' and S.top()=='[':
+        S.pop()
+        print("4",S.data)
+
+
+
+print(S.is_empty())
